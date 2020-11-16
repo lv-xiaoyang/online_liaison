ef4805bb39285e0885812eea352fbb2648cede50
zhaoyi <1113252039@qq.com>
2020-11-16 21:42:56 +0800

    11.16

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,12 @@ Route::get('/index','Index\IndexController@index');
 Route::get('/getcoursetype','Index\IndexController@getcoursetype');//获取首页分类接口
 Route::get('/getIndexcourse','Index\IndexController@getIndexcourse');//获取首页课程接口
 
-
+Route::get('/index/firstCourse','Index\CourseController@getfirstCourse');//获取单个课程信息
+Route::get('/index/getCourseChapter','Index\CourseController@getCourseChapter');//获取章程数据
+Route::get('/index/getCourseSection','Index\CourseController@getCourseSection');//获取节数据
+Route::get('/index/getcourseclass','Index\CourseController@getCourseClass');//获取课时数据
+Route::get('/index/getvideo','Index\CourseController@getvideo');//获取视频数据
+Route::get('/index/getcoursearticle','Index\CourseController@getCourseArticle');//获取课程咨询数据接口
+Route::get('/index/getcoursecorrelation','Index\CourseController@getCoursecorrelation');//获取相关课程数据接口
 
 Route::post('/add','Index\IndexController@add');
